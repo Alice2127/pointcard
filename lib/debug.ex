9ccadb10178027ce108da0bb1dff59a212aa2ca1
Alice2127 <aiokada2128@gmail.com>
2022-06-24 11:46:29 +0900

    feature-debugtool-alice

---
+++ lib/debug.ex
@@ -0,0 +1,12 @@
+defmodule Debug do
+@moduledoc """
+The module for debug.
+When you want to inspect the value of argument, this will be useful.
+"""
+def debugtool(data, msg) do
+  IO.inspect("↓↓↓----#{msg}----↓↓↓")
+  IO.inspect(data)
+  IO.inspect("↑↑↑----#{msg}----↑↑↑")
+end
+
+end
